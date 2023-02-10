e0ed23ab0257b32338f1bea9d3c993977c9bebac
Ricardo Villa <ricardovn8@hotmail.com>
2023-02-10 10:49:09 -0300

    Add project files.

---
+++ Lojinha2/Gerente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Gerente
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public int CodigoFuncionario { get; set; }
+
+        public void Vender()
+        {
+            Console.WriteLine("Este produto foi vendido pelo Gerente, com Sucesso.");
+        }
+
+        public Gerente()
+        {
+            this.Nome = Nome;
+            this.Idade = Idade;
+            this.CodigoFuncionario = CodigoFuncionario;
+        }
+
+    }
+}
